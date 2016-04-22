441230e571f9667fde7d082ca224d947ec5989b6
satguru srivastava <ssatguru@gmail.com>
2016-04-21 20:34:27 -0500

    new asset fences added

--- webapp/assets.js
+++ webapp/assets.js
@@ -1,7 +1,7 @@
 assets={
 	primitives:["plane","box","sphere","disc","cylinder","cone","torus"],
 	skyboxes:["desert","dusk","hilly","rain","TropicalSunnyDay"],
-	structures:["farm_barn_01","farm_silo_01","tipi","huts","coffin"],
+	structures:["farm_barn_01","farm_silo_01","tipi","huts","coffin","fences"],
 	vegetations:["farm_tree_01","farm_stump_01","farm_rocks_01","tree1"],
 	characters:["Joan","Vincent"],
 	vehicles:["farm_tractor_01"],
